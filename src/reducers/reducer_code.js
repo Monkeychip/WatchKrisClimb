67cf49e0c8d011c93a0b61351dfbebe04dc23deb
Monkeychip <argarbarino@gmail.com>
2018-06-21 18:53:10 -0600

    updated fetch_code reducer

--- src/reducers/reducer_code.js
+++ src/reducers/reducer_code.js
@@ -1,8 +1,9 @@
+import { FETCH_CODE } from '../actions/types';
 
 export default function(state = null, action){
 	switch(action.type) {
 
-	case 'FETCH_CODE': //when you call this action type.
+	case FETCH_CODE: //when you call this action type.
 
 		return action.payload;
 	}
